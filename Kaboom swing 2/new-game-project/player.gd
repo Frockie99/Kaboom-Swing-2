54500b47443cf24361a7bdd6c20c95f36ff2e76e
AnotherGameDev <107821679+AnotherGameDev0@users.noreply.github.com>
2025-11-02 18:31:07 +1100

    idk

--- Kaboom swing 2/new-game-project/player.gd
+++ Kaboom swing 2/new-game-project/player.gd
@@ -1,18 +1,33 @@
 extends CharacterBody2D
 
-
 const SPEED = 300.0
 @export var explotionUpwardVelocity = -400.0
 
 
+#the states the player can be in 
+enum playerStates	{
+Standard, # player can do all actions
+dead, # player is dead
+NoControll  # player cant move, use for level finish and stuff
+}
+var playerState # the players current state
+
+func explode()	:
+	pass
+	
+func distanceToGround()	:
+	var space_state = get_world_2d().direct_space_state
+	var query = PhysicsRayQueryParameters2D.create(Vector2(0, 0), Vector2(50, 100))
+
+func _init() -> void:
+	playerState = playerStates.Standard
+
+
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = explotionUpwardVelocity
+	if Input.is_action_just_pressed("ui_accept") and playerState == playerStates.Standard :
+		explode()
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
